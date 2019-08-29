824a1b679c8d2cd9c96a075143747a54336340d7
Edwin Piragauta Vargas <edwin.piragauta@skaphe.com>
2019-08-28 21:10:30 -0500

    Organizacion del código en funciones Adicion de test corregir error en intentos de  clave

--- scriptCountWord.py
+++ scriptCountWord.py
@@ -3,41 +3,59 @@
 # Informatica
 # Willian Pachon
 
+def count_word_app():
 
-msgIntro = "Programa para contar caracteres en una palabra"
-msgInvalidUserPassword = "Usuario o clave invalido"
-msgUnauthorized = "Has consumidos todos tus intentos. NO estas autorizado para utilizar este programa" 
-msgWellcome = "Bienvenido "
-msgIntroduceWord = "Introduce la palabra de interes: "
-msgResultLenWord = "La palabra '%s' tiene: %s caracteres"
-msgThankForParticipation = "Gracias por participar"
+	msgIntro = "Programa para contar caracteres en una palabra"
+	msgInvalidUserPassword = "Usuario o clave invalido, intento No. %s"
+	msgUnauthorized = "Has consumidos todos tus intentos. NO estas autorizado para utilizar este programa" 
+	msgWellcome = "Bienvenido "
+	msgIntroduceWord = "Introduce la palabra de interes: "
+	msgResultLenWord = "La palabra '%s' tiene: %s caracteres"
+	msgThankForParticipation = "Gracias por participar"
 
-print(msgIntro)
+	print(msgIntro)
 
-user = "admin"
-password = "Qwerty123"
+	user = "admin"
+	password = "Qwerty123"
 
-usrInput = raw_input("Usuario: ")
-pwdInput = raw_input("Clave: ")
+	usrInput = raw_input("Usuario: ")
+	pwdInput = raw_input("Clave: ")
 
-maxNumTries = 2
+	maxNumTries = 2
 
-numTry=0
-while pwdInput != password:
-	print(msgInvalidUserPassword)
-	if numTry==2:
-		print(msgUnauthorized)
-		break;
-	pwdInput=str(input("Clave: "))
-	if pwdInput != password:
+	numTry=0
+	while pwdInput != password and usrInput != user:
+		print(msgInvalidUserPassword % (numTry+1))
+		if numTry==2:
+			print(msgUnauthorized)
+			break;
+		pwdInput=str(raw_input("Clave: "))
 		numTry+=1
-		
-if numTry < maxNumTries:
-	print(msgWellcome + usrInput)
+			
+	if numTry < maxNumTries:
+		print(msgWellcome + usrInput)
+	else:
+		return
+				
+	word = raw_input(msgIntroduceWord)
+
+	lenWord = count_letters(word)
+
+	print(msgResultLenWord % (word, lenWord))
+	print(msgThankForParticipation)
+
+def count_letters(word):
+
+	lenWord = len(word)
+	return lenWord
 	
-word = raw_input(msgIntroduceWord)
+def count_letters_test():
+	print "count_letters_test"
+	assert count_letters("test") == 4, "Debe ser 4"
+	
+	
+if __name__ == '__main__':
+    
+	count_letters_test()
+	count_word_app()
 
-lenWord = len(word)
-
-print(msgResultLenWord % (word, lenWord))
-print(msgThankForParticipation)
